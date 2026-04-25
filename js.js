138cc7e41c029e6796cc7cc73ad4b4ab6ad8e5bb
puj16 <putrishidqi@gmail.com>
2026-04-25 15:07:58 +0700

    add more flow to the game

--- js.js
+++ js.js
@@ -1,15 +1,50 @@
-const button = document.querySelector("button");
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const restart = document.getElementById("restart")
+let playerScore=0;
+let computerScore=0;
 
-function getComputerChoice() {
-    const hand=["rock", "paper", "scissors"];
-    const imgSrc =["https://cdn.pixabay.com/photo/2012/04/16/11/07/rock-35522_1280.png",
-        "https://cdn.pixabay.com/photo/2016/10/02/03/14/paper-1708865_1280.png",
-        "https://cdn.pixabay.com/photo/2012/04/26/18/43/scissors-42801_1280.png"    ]
-    const randomValue =Math.random() * 3;
-    const value = Math.floor(randomValue);
+function getComputerChoice(playerSign) {
+    const handSign = new Map([
+        ["rock", "https://cdn.pixabay.com/photo/2012/04/16/11/07/rock-35522_1280.png"],
+        ["paper", "https://cdn.pixabay.com/photo/2016/10/02/03/14/paper-1708865_1280.png"],
+        ["scissors", "https://cdn.pixabay.com/photo/2012/04/26/18/43/scissors-42801_1280.png"]
+    ]);
+    const randomValue =Math.floor(Math.random() * 3);
+    const computerSign = Array.from(handSign.keys())[randomValue];
     const result = document.querySelector("#result");
-    // result.textContent = hand[value];
-    result.innerHTML = `<img src="${imgSrc[value]}" alt="${hand[value]}"><p>${hand[value]}</p>`;
+    const playerHand = document.querySelector("#player-hand");
+    const computerHand = document.querySelector("#computer-hand");
+    playerHand.src = handSign.get(playerSign);
+    computerHand.src = handSign.get(computerSign);
+    if(playerSign === computerSign){
+        result.innerHTML = `<p>It's a tie!</p>`;
+    }else if((playerSign ==="rock"&&computerSign==="scissors")||
+             (playerSign ==="paper"&&computerSign==="rock")||
+             (playerSign ==="scissors"&&computerSign==="paper")){
+        result.innerHTML = `<p>You Win!</p>`;
+        playerScore+=1; 
+        console.log(playerScore) 
+        document.querySelector("#player-score").innerHTML=`<p>Player's score: "${playerScore}"`      
+    }else{
+        result.innerHTML = `<p>You Lose!</p>`;
+        computerScore+=1;
+        document.querySelector("#computer-score").innerHTML=`<p>Computer's score: "${computerScore}"` 
+        console.log(computerScore)      
+    }
+    if(playerScore>=5&&computerScore<5){
+        document.querySelector("#score").innerHTML=`<p> Yo're the Winner!</p> `
+        playerScore=0;
+        restart.style.display="block";
+    }else if(computerScore>=5&&playerScore<5){
+        document.querySelector("#score").innerHTML=`<p> You Lose!</p> `
+        computerScore=0;
+        restart.style.display="block";
+    }
 }
 
-button.addEventListener("click", getComputerChoice);
+rockButton.addEventListener("click", () => getComputerChoice("rock"));
+paperButton.addEventListener("click", () => getComputerChoice("paper"));
+scissorsButton.addEventListener("click", () => getComputerChoice("scissors"));
+restart.addEventListener("click",()=> location.reload());
